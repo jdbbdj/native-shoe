ec9e1fca020c00557a5c5c2ea571ef04963ffe2a
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-02-26 23:57:16 +0800

    components/Cart Header and Footer

---
+++ app/screens/CartScreen/components/CartHeader.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import HeaderSort from "../../../components/HeaderSort";
+const CartHeader = ({}) => {
+  return (
+    <View>
+      <HeaderSort />
+    </View>
+  );
+};
+
+export default CartHeader;
